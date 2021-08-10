f6003ecacab9464b71a5f9b68c9a4a9f759aa809
Rafael Melo <mellotario@gmail.com>
2021-08-10 16:35:59 -0300

    general rule inside and item []

--- src/main/java/csd/refactoring/Trainings.java
+++ src/main/java/csd/refactoring/Trainings.java
@@ -3,54 +3,76 @@ package csd.refactoring;
 public class Trainings {
 
 	public static void main(String[] args) {
-		//int d, int s, int a, Boolean online, String type, int price, int fullPrice
-		Item i1 = new Item(15, 50, 20,true, "CSD", 2000, 2000);
-		Item i2 = new Item(31, 50, 15,true, "CSPO", 4000, 4000);
-		Item i3 = new Item(4, 50, 2,true, "CSM", 2000, 3000);
+		// int d, int s, int a, Boolean online, String type, int price, int fullPrice
+		Item i1 = new Item(15, 50, 20, true, "CSD", 2000, 2000);
+		Item i2 = new Item(31, 50, 15, true, "CSPO", 4000, 4000);
+		Item i3 = new Item(4, 50, 2, true, "CSM", 2000, 3000);
 		Item[] items = new Item[] { i1, i2, i3 };
 		calculate(items);
 	}
 
 	public static void calculate(Item[] items) {
 
-		for (Item training : items) {
-			training.d -= 1;
+		for (int i = 0; i < items.length; i++) {
+			items[i].d -= 1;
 
-			if (training.d <= 1 || training.a <= 3 && training.d <= 5) {
-				training.price = training.fullPrice;
-			}
+			if (items[i].d <= 10) {
 
-			if (training.d <= 10) {
-				if (training.type.equals("CSD")) {
-					training.price = training.fullPrice - (training.d * 30);
+				if (items[i].d <= 1 || items[i].a <= 3 && items[i].d <= 5) {
+					items[i].price = items[i].fullPrice;
 				} else {
-					training.price = training.fullPrice - (training.d * 20);
+					if (items[i].type.equals("CSD")) {
+						items[i].price = items[i].fullPrice - (items[i].d * 30);
+					} else {
+						items[i].price = items[i].fullPrice - (items[i].d * 20);
+					}
 				}
-			} else if (training.d <= 20) {
-				if (training.type.equals("CSD")) {
-					training.price = training.fullPrice - 500;
-				} else if (training.type.equals("CSPO")) {
-					training.price = training.fullPrice - 400;
+
+			} else if (items[i].d <= 20)) {
+				
+				
+				if (items[i].d <= 1 || items[i].a <= 3 && items[i].d <= 5) {
+					items[i].price = items[i].fullPrice;
 				} else {
-					training.price = training.fullPrice - 350;
+					if (items[i].type.equals("CSD")) {
+						items[i].price = items[i].fullPrice - 500;
+					} else {
+						items[i].price = items[i].fullPrice - 400;
+					}
 				}
-			} else if (training.d <= 30) {
-				if (training.type.equals("CSPO")) {
-					training.price = training.fullPrice - 500;
+			} else if (items[i].d <= 30) {
+				if (items[i].d <= 1 || items[i].a <= 3 && items[i].d <= 5) {
+					items[i].price = items[i].fullPrice;
 				} else {
-					training.price = training.fullPrice - 600;
+					if (items[i].type.equals("CSPO")) {
+						items[i].price = items[i].fullPrice - 500;
+					} else {
+						items[i].price = items[i].fullPrice - 600;
+					}
 				}
 			} else {
-				if (training.type.equals("CSM")) {
-					training.price = training.fullPrice - 700;
+				if (items[i].d <= 1 || items[i].a <= 3 && items[i].d <= 5) {
+					items[i].price = items[i].fullPrice;
 				} else {
-					training.price = training.fullPrice - 800;
+					if (items[i].type.equals("CSM")) {
+						items[i].price = items[i].fullPrice - 700;
+					} else {
+						items[i].price = items[i].fullPrice - 800;
+					}
 				}
 			}
 			
-			System.out.println("Preço treinamento SEM desconto:" + training.fullPrice);
-			System.out.println("Preço treinamento COM desconto:" + training.price);
-			System.out.println("Total Vendavel" + training.type + training.a * training.price);
+			if(items[i].type.equals("CSD") && items[i].price > 900) {
+				items[i].price = 900;
+			} else if(items[i].type.equals("CSM") && items[i].price > 1000) {
+				items[i].price = 900;
+			} else 	if(items[i].type.equals("CSPO") && items[i].price > 1200) {
+				items[i].price = 900;
+			}
+
+			System.out.println("Preço treinamento SEM desconto:" + items[i].fullPrice);
+			System.out.println("Preço treinamento COM desconto:" + items[i].price);
+			System.out.println("Total Vendavel" + items[i].type + items[i].a * items[i].price);
 		}
 	}
 }
