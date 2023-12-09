253217e247fa129c732e5b1314d8e11822276a07
Axel Wilhelm Berle  <axelberle@yahoo.de>
2023-12-09 20:14:08 -0300

    tests for the simplified refactoring exercise

---
+++ src/test/java/csd/refactoring/EarlyBirdTests.java
@@ -0,0 +1,92 @@
+package csd.refactoring;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Nested;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class EarlyBirdTests {
+
+
+    @Nested
+    @DisplayName("Business rule: Early Bird with proportional discount 10 days before the training course")
+    class EarlyBirdProportionalDiscount {
+
+        @DisplayName("Proportional discount when 6 days before the training course")
+        @Test
+        void should_apply_proportional_discount_from_day_6_for_CSD() {
+
+            // Arrange
+            /*
+                Days before training course:	6
+                Type:							CSD
+                Full price:						4000
+                Proportional discount:			6*30
+                -->
+                discounted price:		 		4000-(6*30) = 3820
+             */
+            Item i1 = new Item(6+1, 50, 25, true, "CSD", 4000);
+            Item[] items = new Item[]{i1};
+
+            // Act
+            Item[] processedItems = DataProcessor.processData(items);
+
+            // Assert
+            assertEquals(3820, processedItems[0].price, "proportional discount expected");
+        }
+
+        @DisplayName("Proportional discount when 5 days before the training course and enough seats available")
+        @Test
+        void should_apply_proportional_discount_day_5_for_CSD_when_enough_seats_available() {
+
+            // Arrange
+            /*
+                Days before training course:	5
+                Type:							CSD
+                Full price:						4000
+                Proportional discount:			5*30
+                -->
+                discounted:		 				4000-(5*30) = 3850
+             */
+            Item i1 = new Item(5+1, 50, 25, true, "CSD", 4000);
+            Item[] items = new Item[]{i1};
+
+            // Act
+            Item[] processedItems = DataProcessor.processData(items);
+
+            // Assert
+            assertEquals(3850, processedItems[0].price, "proportional discount expected when enough seats available");
+        }
+
+
+
+        @DisplayName("Proportional discount when 10 days before the training course")
+        @ParameterizedTest
+        @ValueSource(strings = {"CSPO", "CSM"})
+        void should_apply_proportional_discount_for_CSM_and_CSPO(String trainingCourseType) {
+
+            // Arrange
+            /*
+                Days before training course:	10
+                Type:							CSPO e CSM
+                Full price:						4000
+                Proportional discount:			10*30
+                -->
+                discounted:		 				4000-(10*20) = 3800
+             */
+            Item i1 = new Item(10+1, 50, 25, true, trainingCourseType, 4000);
+            Item[] items = new Item[]{i1};
+
+            // Act
+            Item[] processedItems = DataProcessor.processData(items);
+
+            // Assert
+            assertEquals(3800, processedItems[0].price, "should apply proportional discount - first day of the interval");
+        }
+
+    }
+
+}
