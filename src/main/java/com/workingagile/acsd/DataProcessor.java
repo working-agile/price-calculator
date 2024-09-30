7756b9158db39778970fee12e6fcaee226a6ac89
Axel <axelberle@yahoo.de>
2024-09-30 15:14:33 -0300

    initial commit

---
+++ src/main/java/com/workingagile/acsd/DataProcessor.java
@@ -0,0 +1,53 @@
+package com.workingagile.acsd;
+
+public class DataProcessor {
+
+	public static int value;
+
+	public static Item[] processData(Item[] itemList) {
+		value = 0;
+
+		for (int i = 0; i < itemList.length; i++) {
+
+			itemList[i].days -= 1;
+
+			if (itemList[i].days <= 10) {
+
+				if (itemList[i].days <= 1 || (itemList[i].available < 3 && itemList[i].days <= 5)) {
+					itemList[i].price = itemList[i].full;
+				} else {
+					if (itemList[i].type.equals("CSD")) {
+						itemList[i].price = itemList[i].full - (itemList[i].days * 30);
+					} else {
+						itemList[i].price = itemList[i].full - (itemList[i].days * 20);
+					}
+				}
+
+			} else if (itemList[i].days > 10) {
+
+				if (itemList[i].days <= 1 || (itemList[i].available < 3 && itemList[i].days <= 5)) {
+					itemList[i].price = itemList[i].full;
+				} else {
+					if (itemList[i].type.equals("CSM")) {
+						itemList[i].price = itemList[i].full - 500;
+					} else {
+						itemList[i].price = itemList[i].full - 400;
+					}
+				}
+			}
+
+			if (itemList[i].type.equals("CSD") && itemList[i].price < 900) {
+				itemList[i].price = 900;
+			} else if (itemList[i].type.equals("CSM") && itemList[i].price < 1000) {
+				itemList[i].price = 1000;
+			} else if (itemList[i].type.equals("CSPO") && itemList[i].price < 1200) {
+				itemList[i].price = 1200;
+			}
+
+			value += (itemList[i].available * itemList[i].price);
+
+		}
+
+		return itemList;
+	}
+}
---
+++ src/main/java/com/workingagile/acsd/Item.java
@@ -0,0 +1,21 @@
+package com.workingagile.acsd;
+
+public class Item {
+	public int days;
+	public int seats;
+	public int available;
+	public Boolean online;
+	public String type;
+	public int price;
+	public int full;
+
+	public Item(int days, int seats, int available, Boolean online, String type, int full) {
+		this.days = days;
+		this.seats = seats;
+		this.available = available;
+		this.online = online;
+		this.type = type;
+		this.price = full;
+		this.full = full;
+	}
+}
