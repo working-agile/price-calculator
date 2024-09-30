dfe750bc16d003c48f6af57dfa2748b7894f7976
Axel <axelberle@yahoo.de>
2024-09-30 17:08:51 -0300

    first scenario added

---
+++ src/test/java/com/workingagile/acsd/MyStepdefs.java
@@ -0,0 +1,26 @@
+package com.workingagile.acsd;
+
+import io.cucumber.datatable.DataTable;
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.Then;
+import io.cucumber.java.en.When;
+
+public class MyStepdefs {
+
+    @Given("a CSD training course:")
+    public void a_csd_training_course(DataTable dataTable) {
+
+        throw new io.cucumber.java.PendingException();
+    }
+    @When("a client asks for a quote on {string}")
+    public void a_client_asks_for_a_quote_on(String string) {
+
+        throw new io.cucumber.java.PendingException();
+    }
+    @Then("the discounted price should be {string}")
+    public void the_discounted_price_should_be(String string) {
+
+        throw new io.cucumber.java.PendingException();
+    }
+
+}
---
+++ src/test/java/com/workingagile/acsd/RunCucumberTest.java
@@ -0,0 +1,15 @@
+package com.workingagile.acsd;
+
+import org.junit.platform.suite.api.ConfigurationParameter;
+import org.junit.platform.suite.api.IncludeEngines;
+import org.junit.platform.suite.api.SelectPackages;
+import org.junit.platform.suite.api.Suite;
+
+import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectPackages("com.workingagile.acsd")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+public class RunCucumberTest {
+}
