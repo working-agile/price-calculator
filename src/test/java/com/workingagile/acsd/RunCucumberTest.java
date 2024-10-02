9d8a8984c091316bb35abbeb0f4ad5de277e2557
Axel <axelberle@yahoo.de>
2024-10-02 09:22:59 -0300

    included basic reporting

--- src/test/java/com/workingagile/acsd/RunCucumberTest.java
+++ src/test/java/com/workingagile/acsd/RunCucumberTest.java
@@ -10,6 +10,8 @@ import static io.cucumber.junit.platform.engine.Constants.PLUGIN_PROPERTY_NAME;
 @Suite
 @IncludeEngines("cucumber")
 @SelectPackages("com.workingagile.acsd")
-@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty")
+@ConfigurationParameter(key = PLUGIN_PROPERTY_NAME, value = "pretty, html:target/cucumber-report/cucumber.html," +
+        "json:target/cucumber-report/cucumber.json"
+)
 public class RunCucumberTest {
 }
