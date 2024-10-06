4d67304b98a46bc05328baffd481d15562b01725
Axel <axelberle@yahoo.de>
2024-10-06 18:01:04 -0300

    clean code naming

--- src/main/java/com/workingagile/acsd/DataProcessor.java
+++ src/main/java/com/workingagile/acsd/DataProcessor.java
@@ -10,62 +10,62 @@ public class DataProcessor {
 
 		remainingSalesTarget = 0;
 
-		for (int i = 0; i < scheduledTrainingCourses.length; i++) {
+		for (TrainingCourse trainingCourse: scheduledTrainingCourses) {
 
-			if (isBeforeScheduledDate(i) && moveToNextDay) {
-				scheduledTrainingCourses[i].daysBeforeTrainingCourse--;
+			if (isBeforeScheduledDate(trainingCourse) && moveToNextDay) {
+				trainingCourse.daysBeforeTrainingCourse--;
 			}
 
-			if (isProportionalEarlyBird(i)) {
-				if (fullPricePolicyApplies(i)) {
-					scheduledTrainingCourses[i].currentDiscountedPrice = scheduledTrainingCourses[i].fullPrice;
+			if (isProportionalEarlyBird(trainingCourse)) {
+				if (fullPricePolicyApplies(trainingCourse)) {
+					trainingCourse.currentDiscountedPrice = trainingCourse.fullPrice;
 				} else {
-					if (scheduledTrainingCourses[i].type.equals("CSD")) {
-						scheduledTrainingCourses[i].currentDiscountedPrice = scheduledTrainingCourses[i].fullPrice - (scheduledTrainingCourses[i].daysBeforeTrainingCourse * 30);
+					if (trainingCourse.type.equals("CSD")) {
+						trainingCourse.currentDiscountedPrice = trainingCourse.fullPrice - (trainingCourse.daysBeforeTrainingCourse * 30);
 					} else {
-						scheduledTrainingCourses[i].currentDiscountedPrice = scheduledTrainingCourses[i].fullPrice - (scheduledTrainingCourses[i].daysBeforeTrainingCourse * 20);
+						trainingCourse.currentDiscountedPrice = trainingCourse.fullPrice - (trainingCourse.daysBeforeTrainingCourse * 20);
 					}
 				}
 
-			} else if (isSuperEarlyBird(i)) {
+			} else if (isSuperEarlyBird(trainingCourse)) {
 
-				if (fullPricePolicyApplies(i)) {
-					scheduledTrainingCourses[i].currentDiscountedPrice = scheduledTrainingCourses[i].fullPrice;
+				if (fullPricePolicyApplies(trainingCourse)) {
+					trainingCourse.currentDiscountedPrice = trainingCourse.fullPrice;
 				} else {
-					if (scheduledTrainingCourses[i].type.equals("CSM")) {
-						scheduledTrainingCourses[i].currentDiscountedPrice = scheduledTrainingCourses[i].fullPrice - 500;
+					if (trainingCourse.type.equals("CSM")) {
+						trainingCourse.currentDiscountedPrice = trainingCourse.fullPrice - 500;
 					} else {
-						scheduledTrainingCourses[i].currentDiscountedPrice = scheduledTrainingCourses[i].fullPrice - 400;
+						trainingCourse.currentDiscountedPrice = trainingCourse.fullPrice - 400;
 					}
 				}
 			}
 
-			if (scheduledTrainingCourses[i].type.equals("CSD") && scheduledTrainingCourses[i].currentDiscountedPrice < 900) {
-				scheduledTrainingCourses[i].currentDiscountedPrice = 900;
-			} else if (scheduledTrainingCourses[i].type.equals("CSM") && scheduledTrainingCourses[i].currentDiscountedPrice < 1000) {
-				scheduledTrainingCourses[i].currentDiscountedPrice = 1000;
-			} else if (scheduledTrainingCourses[i].type.equals("CSPO") && scheduledTrainingCourses[i].currentDiscountedPrice < 1200) {
-				scheduledTrainingCourses[i].currentDiscountedPrice = 1200;
+			if (trainingCourse.type.equals("CSD") && trainingCourse.currentDiscountedPrice < 900) {
+				trainingCourse.currentDiscountedPrice = 900;
+			} else if (trainingCourse.type.equals("CSM") && trainingCourse.currentDiscountedPrice < 1000) {
+				trainingCourse.currentDiscountedPrice = 1000;
+			} else if (trainingCourse.type.equals("CSPO") && trainingCourse.currentDiscountedPrice < 1200) {
+				trainingCourse.currentDiscountedPrice = 1200;
 			}
 
-			remainingSalesTarget += (scheduledTrainingCourses[i].remainingAvailableSeats * scheduledTrainingCourses[i].currentDiscountedPrice);
+			remainingSalesTarget += (trainingCourse.remainingAvailableSeats * trainingCourse.currentDiscountedPrice);
 
 		}
 	}
 
-	private static boolean fullPricePolicyApplies(int i) {
-		return scheduledTrainingCourses[i].daysBeforeTrainingCourse <= 1 || (scheduledTrainingCourses[i].remainingAvailableSeats < 3 && scheduledTrainingCourses[i].daysBeforeTrainingCourse <= 5);
+	private static boolean fullPricePolicyApplies(TrainingCourse trainingCourse) {
+		return trainingCourse.daysBeforeTrainingCourse <= 1 || (trainingCourse.remainingAvailableSeats < 3 && trainingCourse.daysBeforeTrainingCourse <= 5);
 	}
 
-	private static boolean isBeforeScheduledDate(int i) {
-		return scheduledTrainingCourses[i].daysBeforeTrainingCourse > 0;
+	private static boolean isBeforeScheduledDate(TrainingCourse trainingCourse) {
+		return trainingCourse.daysBeforeTrainingCourse > 0;
 	}
 
-	private static boolean isSuperEarlyBird(int i) {
-		return scheduledTrainingCourses[i].daysBeforeTrainingCourse > 10;
+	private static boolean isSuperEarlyBird(TrainingCourse trainingCourse) {
+		return trainingCourse.daysBeforeTrainingCourse > 10;
 	}
 
-	private static boolean isProportionalEarlyBird(int i) {
-		return scheduledTrainingCourses[i].daysBeforeTrainingCourse <= 10;
+	private static boolean isProportionalEarlyBird(TrainingCourse trainingCourse) {
+		return trainingCourse.daysBeforeTrainingCourse <= 10;
 	}
 }
