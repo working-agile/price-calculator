78ee0f46789eb7719e8ba47a9336cbe904c40b98
Axel <axelberle@yahoo.de>
2024-12-01 18:29:15 -0300

    setup for refactoring exercise - database dependency

--- src/main/java/com/workingagile/acsd/DataProcessor.java
+++ src/main/java/com/workingagile/acsd/DataProcessor.java
@@ -2,19 +2,20 @@ package com.workingagile.acsd;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DataProcessor {
 
-	public static int salesValue;
+	public int salesValue;
 
-	public static ArrayList<Item> list;
+	public ArrayList<Item> list;
+
+	// ----------------------------------------------------
 
 	private static Connection database;
 
 	private static DataProcessor theSingleton;
 
-	protected DataProcessor() {
+	private DataProcessor() {
 		System.out.println("Initializing DataProcessor. Very slow...");
 		try {
 			Thread.sleep(1000);
@@ -26,133 +27,101 @@ public class DataProcessor {
 		return theSingleton;
 	}
 
-	public static void calculateData(boolean next) {
-		getInstance().calculateDataInstance(next);
-	}
+	public void calculateData(boolean next) {
 
-	protected void calculateDataInstance(boolean next) {
-
-		createDatabaseConnection();
-
-		try {
-
-			salesValue = 0;
-
-			List<Item> items = getItemsFromDatabase();
-
-			List<Item> newList = processItems(next, items);
-
-			updateItemsInDatabase(newList);
-
-			list = new ArrayList<Item>(newList);
-
-		} catch(Exception exception) {}
-	}
-
-	protected List<Item> processItems(boolean next, List<Item> items) {
-		List <Item> newList = new ArrayList<Item>();
-		for (Item item: items) {
-			processItem(next, item, newList);
-		}
-		return newList;
-	}
-
-	protected void processItem(boolean next, Item item, List<Item> newList) {
-		if (!(item.days < 0 || (next && item.days == 0))) {
-
-			newList.add(item);
-
-			if (next) {
-				item.days--;
-			}
-
-			if (item.days <= 10) {
-
-				if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
-					item.curr = item.full;
-				} else {
-					if (item.type.equals("CSD")) {
-						item.curr = item.full - (item.days * 30);
-					} else {
-						item.curr = item.full - (item.days * 20);
-					}
-				}
-
-			} else if (item.days > 10) {
-
-				if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
-					item.curr = item.full;
-				} else {
-					if (item.type.equals("CSM")) {
-						item.curr = item.full - 500;
-					} else {
-						item.curr = item.full - 400;
-					}
-				}
-			}
-
-			if (item.type.equals("CSD") && item.curr < 900) {
-				item.curr = 900;
-			} else if (item.type.equals("CSM") && item.curr < 1000) {
-				item.curr = 1000;
-			} else if (item.type.equals("CSPO") && item.curr < 1200) {
-				item.curr = 1200;
-			}
-
-			salesValue += (item.avail * item.curr);
-
-		}
-	}
-
-	private static List<Item> getItemsFromDatabase() throws SQLException {
-		String query = "select * from tr_crs";
-		Statement st = database.createStatement();
-		ResultSet rs = st.executeQuery(query);
-
-		List<Item> itemList = new ArrayList<>();
-		while (rs.next()) {
-
-			long id = rs.getLong("id");
-			String trDate = rs.getString("tr_date");
-			int days = rs.getInt("days");
-			int ttlSeats = rs.getInt("ttl_seats");
-			int avail = rs.getInt("avail");
-			String type = rs.getString("type");
-			int curr = rs.getInt("curr");
-			int full = rs.getInt("full");
-
-			Item item = new Item(id, trDate, days, ttlSeats, avail, type, curr, full);
-			itemList.add(item);
-		}
-		st.close();
-		return itemList;
-	}
-
-	private void updateItemsInDatabase(List<Item> newList) throws SQLException {
-		for (int i = 0; i< newList.size(); i++) {
-
-			String update = "update tr_crs set days=?, curr=? where id=? ";
-			PreparedStatement pstmt = database.prepareStatement(update);
-
-			pstmt.setInt(1, newList.get(i).days);
-			pstmt.setInt(2, newList.get(i).curr);
-			pstmt.setLong(3, newList.get(i).id);
-
-			int res = pstmt.executeUpdate();
-			System.out.println("Result: " + res);
-		}
-	}
-
-	private void createDatabaseConnection() {
 		if (database == null) {
 			try {
-				String url = "jdbc:mysql://production-database:3306/prices";
-				Class.forName("com.mysql.cj.jdbc.Driver");
+				String url = "jdbc:postgresql://127.0.0.1:5432/a-csd";
+				Class.forName("org.postgresql.Driver");
 				database = DriverManager.getConnection(url, "prices-user", "prices-secret-password");
 			} catch (Exception e) {
 			}
 		}
-	}
+		// read the prices
+		try {
+			String query = "select * from tr_crs";
+			Statement st = database.createStatement();
+			ResultSet rs = st.executeQuery(query);
 
+			salesValue = 0;
+			ArrayList<Item> newList = new ArrayList<>();
+
+			while (rs.next()) {
+
+				long id = rs.getLong("id");
+				String trDate = rs.getString("tr_date");
+				int days = rs.getInt("days");
+				int ttlSeats = rs.getInt("ttl_seats");
+				int avail = rs.getInt("avail");
+				String type = rs.getString("type");
+				int curr = rs.getInt("curr");
+				int full = rs.getInt("full");
+
+				Item item = new Item(id, trDate, days, ttlSeats, avail, type, curr, full);
+
+				if (!(item.days < 0 || (next && item.days == 0))) {
+
+					newList.add(item);
+
+					if (next) {
+						item.days--;
+					}
+
+					if (item.days <= 10) {
+
+						if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
+							item.curr = item.full;
+						} else {
+							if (item.type.equals("CSD")) {
+								item.curr = item.full - (item.days * 30);
+							} else {
+								item.curr = item.full - (item.days * 20);
+							}
+						}
+
+					} else if (item.days > 10) {
+
+						if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
+							item.curr = item.full;
+						} else {
+							if (item.type.equals("CSM")) {
+								item.curr = item.full - 500;
+							} else {
+								item.curr = item.full - 400;
+							}
+						}
+					}
+
+					if (item.type.equals("CSD") && item.curr < 900) {
+						item.curr = 900;
+					} else if (item.type.equals("CSM") && item.curr < 1000) {
+						item.curr = 1000;
+					} else if (item.type.equals("CSPO") && item.curr < 1200) {
+						item.curr = 1200;
+					}
+
+					salesValue += (item.avail * item.curr);
+
+				}
+			}
+			st.close();
+
+			// update database
+			for (int i=0; i<newList.size(); i++) {
+
+				String update = "update tr_crs set days=?, curr=? where id=? ";
+				PreparedStatement pstmt = database.prepareStatement(update);
+
+				pstmt.setInt(1, newList.get(i).days);
+				pstmt.setInt(2, newList.get(i).curr);
+				pstmt.setLong(3, newList.get(i).id);
+
+				int res = pstmt.executeUpdate();
+				System.out.println("Result: " + res);
+			}
+			list = newList;
+
+		} catch(Exception exception) {}
+	}
 
 }
--- src/test/java/com/workingagile/acsd/DataProcessorTests.java
+++ src/test/java/com/workingagile/acsd/DataProcessorTests.java
@@ -2,71 +2,82 @@ package com.workingagile.acsd;
 
 import org.junit.jupiter.api.Test;
 
-import java.util.ArrayList;
-import java.util.List;
-
 public class DataProcessorTests {
+
+
+    @Test
+    void insert_data_into_database() {
+
+        // database: a-csd
+        // user: "prices-admin"
+        // password: "prices-secret-password"
+        // url = "jdbc:mysql://a-csd-database:3306/prices"
+
+        // table: tr_crs
+        // long: "id"
+        // string: "tr_date"
+        // int: "days"
+        // int: "ttl_seats"
+        // int: "avail"
+        // string: "type"
+        // int: "curr"
+        // int: full
+
+        // current date: 1. January 2025
+
+        // Item("9 January 2025",   9,  30, 7, "CSPO", 4000, 4000));
+        // Item("10 January 2025",  10, 30, 8, "CSD", 3000, 3000));
+        // Item("20 January 2025",  20, 30, 27, "CSM", 3000, 3000));
+
+    }
+
+
 
     @Test
     void manual() {
 
-        // Refactor so that I can execute a test with
-        // the new functionalities
-        //
-        // - add new functionality at a big method: calculateData()
-        //
-        // but the method is static
+        // 1. Show the current prices
+        DataProcessor processor = DataProcessor.getInstance();
+        processor.calculateData(false);
 
-        /*
-        List items = new ArrayList<Item>();
-        items.add(new Item(1,"10 January 2025",10, 30, 8, "CSD", 3000, 3000));
-        items.add(new Item(2,"9 January 2025",9, 30, 7, "CSPO", 4000, 4000));
-        items.add(new Item(3, "20 January 2025", 20, 30, 27, "CSM", 3000, 3000));
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Total sales target remaining: " + processor.salesValue);
+        System.out.println("-----------------------------------------------------");
 
-        TestableDataProcessor dataProcessor = new TestableDataProcessor();
-
-        List<Item> updatedItems = dataProcessor.calculateData(items, false);
-
-        System.out.println("Current training courses:");
-
-        for (Item trainingCourse: updatedItems) {
+        for (Item item: processor.list) {
+            System.out.println("Current training courses:");
             System.out.println("------------------------------");
             System.out.println("Training course");
-            System.out.println("Type: " + trainingCourse.type);
-            System.out.println("When: " + trainingCourse.trDate);
-            System.out.println("Remaining days before training course: " + trainingCourse.days);
-            System.out.println("Full Price: " + trainingCourse.full);
-            System.out.println("Current price: " + trainingCourse.curr);
-            System.out.println("Number of seats: " + trainingCourse.seats);
-            System.out.println("Remaining available seats: " + trainingCourse.avail);
+            System.out.println("Type: " + item.type);
+            System.out.println("When: " + item.trDate);
+            System.out.println("Remaining days before training course: " + item.days);
+            System.out.println("Full Price: " + item.full);
+            System.out.println("Current price: " + item.curr);
+            System.out.println("Number of seats: " + item.seats);
+            System.out.println("Remaining available seats: " + item.avail);
         }
-        System.out.println("-----------------------------------------------------");
-        System.out.println("Total sales target remaining: " + DataProcessor.salesValue);
-        System.out.println("-----------------------------------------------------");
 
+        // 2. Move to next day
         System.out.println("\n\nMove to next day");
+        processor.calculateData(true);
 
-        DataProcessor.calculateData(true);
+        System.out.println("-----------------------------------------------------");
+        System.out.println("Total sales target remaining: " + processor.salesValue);
+        System.out.println("-----------------------------------------------------");
 
-        System.out.println("Updated training courses...");
-
-        for (Item trainingCourse: updatedItems) {
+        for (Item item: processor.list) {
+            System.out.println("Current training courses:");
             System.out.println("------------------------------");
             System.out.println("Training course");
-            System.out.println("Type: " + trainingCourse.type);
-            System.out.println("When: " + trainingCourse.trDate);
-            System.out.println("Remaining days before training course: " + trainingCourse.days);
-            System.out.println("Full Price: " + trainingCourse.full);
-            System.out.println("Current price: " + trainingCourse.curr);
-            System.out.println("Number of seats: " + trainingCourse.seats);
-            System.out.println("Remaining available seats: " + trainingCourse.avail);
+            System.out.println("Type: " + item.type);
+            System.out.println("When: " + item.trDate);
+            System.out.println("Remaining days before training course: " + item.days);
+            System.out.println("Full Price: " + item.full);
+            System.out.println("Current price: " + item.curr);
+            System.out.println("Number of seats: " + item.seats);
+            System.out.println("Remaining available seats: " + item.avail);
         }
 
-        System.out.println("-----------------------------------------------------");
-        System.out.println("Total sales target remaining: " + DataProcessor.salesValue);
-        System.out.println("-----------------------------------------------------");
-
-        */
     }
 
 }
