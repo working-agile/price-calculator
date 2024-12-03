e45a94195251f5a7132bc4db4f568be4e673b20a
Axel <axelberle@yahoo.de>
2024-12-03 11:07:35 -0300

    separated business logic from infrastructure code

--- src/main/java/com/workingagile/acsd/DataProcessor.java
+++ src/main/java/com/workingagile/acsd/DataProcessor.java
@@ -2,6 +2,7 @@ package com.workingagile.acsd;
 
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class DataProcessor {
 
@@ -23,13 +24,121 @@ public class DataProcessor {
 			try {
 				Thread.sleep(1000);
 			} catch (InterruptedException ex) {}
+			initDatabaseConnection();
 			theSingleton = new DataProcessor();
 		}
 		return theSingleton;
 	}
 
 	public void calculateData(boolean next) {
+		try {
 
+			List<Item> currentTrainingCourses = loadTrainingCoursesFromDatabase();
+
+			ArrayList<Item> newList = processTrainingCourses(next, currentTrainingCourses);
+
+			updateTrainingCoursesInDatabase(newList);
+
+			list = newList;
+
+		} catch(Exception exception) {}
+	}
+
+	private ArrayList<Item> processTrainingCourses(boolean next, List<Item> currentTrainingCourses) {
+		salesValue = 0;
+		ArrayList<Item> newList = new ArrayList<>();
+		for (Item currentItem: currentTrainingCourses) {
+			processTrainingCourse(next, currentItem, newList);
+		}
+		return newList;
+	}
+
+	private List<Item> loadTrainingCoursesFromDatabase() throws SQLException {
+		String query = "select * from tr_crs";
+		Statement st = database.createStatement();
+		ResultSet rs = st.executeQuery(query);
+		List<Item> currentTrainingCourses = new ArrayList<>();
+		while (rs.next()) {
+
+			long id = rs.getLong("id");
+			String trDate = rs.getString("tr_date");
+			int days = rs.getInt("days");
+			int ttlSeats = rs.getInt("ttl_seats");
+			int avail = rs.getInt("avail");
+			String type = rs.getString("type");
+			int curr = rs.getInt("curr_price");
+			int full = rs.getInt("full_price");
+
+			Item item = new Item(id, trDate, days, ttlSeats, avail, type, curr, full);
+
+			currentTrainingCourses.add(item);
+		}
+		st.close();
+		return currentTrainingCourses;
+	}
+
+	private static void updateTrainingCoursesInDatabase(ArrayList<Item> newList) throws SQLException {
+		for (int i = 0; i< newList.size(); i++) {
+
+			String update = "update tr_crs set days=?, curr_price=? where id=? ";
+			PreparedStatement pstmt = database.prepareStatement(update);
+
+			pstmt.setInt(1, newList.get(i).days);
+			pstmt.setInt(2, newList.get(i).curr);
+			pstmt.setLong(3, newList.get(i).id);
+
+			int res = pstmt.executeUpdate();
+		}
+	}
+
+	private void processTrainingCourse(boolean next, Item item, ArrayList<Item> newList) {
+		if (!(item.days < 0 || (next && item.days == 0))) {
+
+			newList.add(item);
+
+			if (next) {
+				item.days--;
+			}
+
+			if (item.days <= 10) {
+
+				if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
+					item.curr = item.full;
+				} else {
+					if (item.type.equals("CSD")) {
+						item.curr = item.full - (item.days * 30);
+					} else {
+						item.curr = item.full - (item.days * 20);
+					}
+				}
+
+			} else if (item.days > 10) {
+
+				if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
+					item.curr = item.full;
+				} else {
+					if (item.type.equals("CSM")) {
+						item.curr = item.full - 500;
+					} else {
+						item.curr = item.full - 400;
+					}
+				}
+			}
+
+			if (item.type.equals("CSD") && item.curr < 900) {
+				item.curr = 900;
+			} else if (item.type.equals("CSM") && item.curr < 1000) {
+				item.curr = 1000;
+			} else if (item.type.equals("CSPO") && item.curr < 1200) {
+				item.curr = 1200;
+			}
+
+			salesValue += (item.avail * item.curr);
+
+		}
+	}
+
+	private static void initDatabaseConnection() {
 		if (database == null) {
 			try {
 				String url = "jdbc:postgresql://127.0.0.1:5432/postgres";
@@ -38,90 +147,6 @@ public class DataProcessor {
 			} catch (Exception e) {
 			}
 		}
-		// read the prices
-		try {
-			String query = "select * from tr_crs";
-			Statement st = database.createStatement();
-			ResultSet rs = st.executeQuery(query);
-
-			salesValue = 0;
-			ArrayList<Item> newList = new ArrayList<>();
-
-			while (rs.next()) {
-
-				long id = rs.getLong("id");
-				String trDate = rs.getString("tr_date");
-				int days = rs.getInt("days");
-				int ttlSeats = rs.getInt("ttl_seats");
-				int avail = rs.getInt("avail");
-				String type = rs.getString("type");
-				int curr = rs.getInt("curr_price");
-				int full = rs.getInt("full_price");
-
-				Item item = new Item(id, trDate, days, ttlSeats, avail, type, curr, full);
-
-				if (!(item.days < 0 || (next && item.days == 0))) {
-
-					newList.add(item);
-
-					if (next) {
-						item.days--;
-					}
-
-					if (item.days <= 10) {
-
-						if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
-							item.curr = item.full;
-						} else {
-							if (item.type.equals("CSD")) {
-								item.curr = item.full - (item.days * 30);
-							} else {
-								item.curr = item.full - (item.days * 20);
-							}
-						}
-
-					} else if (item.days > 10) {
-
-						if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
-							item.curr = item.full;
-						} else {
-							if (item.type.equals("CSM")) {
-								item.curr = item.full - 500;
-							} else {
-								item.curr = item.full - 400;
-							}
-						}
-					}
-
-					if (item.type.equals("CSD") && item.curr < 900) {
-						item.curr = 900;
-					} else if (item.type.equals("CSM") && item.curr < 1000) {
-						item.curr = 1000;
-					} else if (item.type.equals("CSPO") && item.curr < 1200) {
-						item.curr = 1200;
-					}
-
-					salesValue += (item.avail * item.curr);
-
-				}
-			}
-			st.close();
-
-			// update database
-			for (int i=0; i<newList.size(); i++) {
-
-				String update = "update tr_crs set days=?, curr_price=? where id=? ";
-				PreparedStatement pstmt = database.prepareStatement(update);
-
-				pstmt.setInt(1, newList.get(i).days);
-				pstmt.setInt(2, newList.get(i).curr);
-				pstmt.setLong(3, newList.get(i).id);
-
-				int res = pstmt.executeUpdate();
-			}
-			list = newList;
-
-		} catch(Exception exception) {}
 	}
 
 }
