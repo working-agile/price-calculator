8c41b667b0143367a61e2e6c9f42bac188f42191
Axel <axelberle@yahoo.de>
2025-04-19 16:19:18 -0300

    remove setTestConnection to make it more realworld

---
+++ src/test/java/com/workingagile/acsd/UnitTests.java
@@ -0,0 +1,27 @@
+package com.workingagile.acsd;
+
+import com.workingagile.acsd.backend.DataProcessor;
+import com.workingagile.acsd.backend.Item;
+import org.junit.jupiter.api.Test;
+import java.util.List;
+
+public class UnitTests {
+
+    @Test
+    public void some_unit_test() {
+
+        // Arrange
+        DataProcessor processor = DataProcessor.getInstance();
+        // TODO insert data
+
+        // Act - call the function
+        processor.calculateData(false);
+
+        // Assert
+        List<Item> actualResult = processor.list;
+        // TODO compare with the expected result
+
+    }
+
+
+}
