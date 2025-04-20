3d256d592c44300d38d3e1883c954b37514020a3
Axel <axelberle@yahoo.de>
2025-04-20 12:35:06 -0300

    break into smaller methods, separating concerns

--- src/main/java/com/workingagile/acsd/backend/RefactoredDataProcessor.java
+++ src/main/java/com/workingagile/acsd/backend/RefactoredDataProcessor.java
@@ -1,7 +1,10 @@
 package com.workingagile.acsd.backend;
 
+import org.jetbrains.annotations.Nullable;
+
 import java.sql.*;
 import java.util.ArrayList;
+import java.util.List;
 
 public class RefactoredDataProcessor extends DataProcessor {
 
@@ -11,26 +14,34 @@ public class RefactoredDataProcessor extends DataProcessor {
 
         Connection database = getDatabaseConnection();
 
-        if (database == null) {
-            try {
+        database = initDatabaseConnection(database);
 
-                String url = "jdbc:postgresql://127.0.0.1:5432/production_database";
-                Class.forName("org.postgresql.Driver");
-                database = DriverManager.getConnection(url, "postgres", "postgres");
+        ArrayList<Item> trainingCourses = readTrainingCoursesFromDatabase(database);
 
-            } catch (Exception e) {
-            }
+        processTrainingCourses(trainingCourses, moveToNextDay);
+
+        updatePricesInDatabase(trainingCourses, database);
+
+        list = trainingCourses;
+
+    }
+
+    private void processTrainingCourses(List<Item> trainingCourses, boolean moveToNextDay) {
+
+        salesValue = 0;
+
+        for (Item item: trainingCourses) {
+            processTrainingCourse(moveToNextDay, item);
         }
+    }
 
-
-        // read the prices from database
-        try {
+    private ArrayList<Item> readTrainingCoursesFromDatabase(Connection database) {
+        ArrayList<Item> newList = new ArrayList<>();
+        Statement st = null;
+        try  {
             String query = "select * from tr_crs";
-            Statement st = database.createStatement();
+            st = database.createStatement();
             ResultSet rs = st.executeQuery(query);
-
-            salesValue = 0;
-            ArrayList<Item> newList = new ArrayList<>();
 
             while (rs.next()) {
 
@@ -44,56 +55,35 @@ public class RefactoredDataProcessor extends DataProcessor {
                 int full = rs.getInt("full_price");
 
                 Item item = new Item(id, trDate, days, ttlSeats, avail, type, curr, full);
-
-                if (!(item.days < 0 || (moveToNextDay && item.days == 0))) {
-
-                    newList.add(item);
-
-                    if (moveToNextDay) {
-                        item.days--;
-                    }
-
-                    if (item.days <= 10) {
-
-                        if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
-                            item.curr = item.full;
-                        } else {
-                            if (item.type.equals("CSD")) {
-                                item.curr = item.full - (item.days * 30);
-                            } else {
-                                item.curr = item.full - (item.days * 20);
-                            }
-                        }
-
-                    } else if (item.days > 10) {
-
-                        if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
-                            item.curr = item.full;
-                        } else {
-                            if (item.type.equals("CSM")) {
-                                item.curr = item.full - 500;
-                            } else {
-                                item.curr = item.full - 400;
-                            }
-                        }
-                    }
-
-                    if (item.type.equals("CSD") && item.curr < 900) {
-                        item.curr = 900;
-                    } else if (item.type.equals("CSM") && item.curr < 1000) {
-                        item.curr = 1000;
-                    } else if (item.type.equals("CSPO") && item.curr < 1200) {
-                        item.curr = 1200;
-                    }
-
-                    salesValue += (item.avail * item.curr);
-
-                }
+                newList.add(item);
             }
             st.close();
 
-            // update prices in database
-            for (int i=0; i<newList.size(); i++) {
+            return newList;
+
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
+
+    private static @Nullable Connection initDatabaseConnection(Connection database) {
+        if (database == null) {
+            try {
+
+                String url = "jdbc:postgresql://127.0.0.1:5432/production_database";
+                Class.forName("org.postgresql.Driver");
+                database = DriverManager.getConnection(url, "postgres", "postgres");
+
+            } catch (Exception e) {
+                throw new RuntimeException(e);
+            }
+        }
+        return database;
+    }
+
+    private static void updatePricesInDatabase(ArrayList<Item> newList, Connection database) {
+        try {
+            for (int i = 0; i< newList.size(); i++) {
 
                 String update = "update tr_crs set days=?, curr_price=? where id=? ";
                 PreparedStatement pstmt = database.prepareStatement(update);
@@ -104,13 +94,54 @@ public class RefactoredDataProcessor extends DataProcessor {
 
                 int res = pstmt.executeUpdate();
             }
-            list = newList;
+        } catch (Exception e) {
+            throw new RuntimeException(e);
+        }
+    }
 
-        } catch(Exception exception) {}
+    private void processTrainingCourse(boolean moveToNextDay, Item item) {
+        if (!(item.days < 0 || (moveToNextDay && item.days == 0))) {
 
+            if (moveToNextDay) {
+                item.days--;
+            }
 
+            if (item.days <= 10) {
 
+                if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
+                    item.curr = item.full;
+                } else {
+                    if (item.type.equals("CSD")) {
+                        item.curr = item.full - (item.days * 30);
+                    } else {
+                        item.curr = item.full - (item.days * 20);
+                    }
+                }
 
+            } else if (item.days > 10) {
+
+                if (item.days <= 1 || (item.avail < 3 && item.days <= 5)) {
+                    item.curr = item.full;
+                } else {
+                    if (item.type.equals("CSM")) {
+                        item.curr = item.full - 500;
+                    } else {
+                        item.curr = item.full - 400;
+                    }
+                }
+            }
+
+            if (item.type.equals("CSD") && item.curr < 900) {
+                item.curr = 900;
+            } else if (item.type.equals("CSM") && item.curr < 1000) {
+                item.curr = 1000;
+            } else if (item.type.equals("CSPO") && item.curr < 1200) {
+                item.curr = 1200;
+            }
+
+            salesValue += (item.avail * item.curr);
+
+        }
     }
 
 }
