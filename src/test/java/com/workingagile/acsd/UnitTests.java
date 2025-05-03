1fed7c59015f4a8419a65792293d107e11dcdd63
Axel <axelberle@yahoo.de>
2025-05-03 17:01:00 -0300

    disable template test

--- src/test/java/com/workingagile/acsd/UnitTests.java
+++ src/test/java/com/workingagile/acsd/UnitTests.java
@@ -2,11 +2,13 @@ package com.workingagile.acsd;
 
 import com.workingagile.acsd.backend.DataProcessor;
 import com.workingagile.acsd.backend.TrainingCourse;
+import org.junit.jupiter.api.Disabled;
 import org.junit.jupiter.api.Test;
 import java.util.List;
 
 public class UnitTests {
 
+    @Disabled
     @Test
     public void some_unit_test() {
 
