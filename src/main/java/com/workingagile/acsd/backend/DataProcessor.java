0096b234bd695d6daf15f85645402e9961f487a0
Axel Wilhelm Berle <axelberle@yahoo.de>
2025-06-16 16:35:38 +0000

    some clean up

--- src/main/java/com/workingagile/acsd/backend/DataProcessor.java
+++ src/main/java/com/workingagile/acsd/backend/DataProcessor.java
@@ -2,7 +2,6 @@ package com.workingagile.acsd.backend;
 
 import java.sql.*;
 import java.util.ArrayList;
-import java.util.List;
 
 public class DataProcessor {
 
@@ -51,9 +50,7 @@ public class DataProcessor {
 			String query = "select * from tr_crs";
 			Statement st = database.createStatement();
 			ResultSet rs = st.executeQuery(query);
-
-			ArrayList<Item> newList = new ArrayList<>();
-
+			
 			while (rs.next()) {
 
 				long id = rs.getLong("id");
