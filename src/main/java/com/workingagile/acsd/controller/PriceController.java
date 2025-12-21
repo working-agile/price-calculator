2009794e29c862e47f522cb95f645f6383236120
Axel Wilhelm Berle <axelberle@yahoo.de>
2025-12-21 13:18:08 -0300

    add additional endpoints for inserting, updating

--- src/main/java/com/workingagile/acsd/controller/PriceController.java
+++ src/main/java/com/workingagile/acsd/controller/PriceController.java
@@ -2,35 +2,33 @@ package com.workingagile.acsd.controller;
 
 import com.workingagile.acsd.backend.DataProcessor;
 import com.workingagile.acsd.backend.Item;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PutMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
 import java.util.List;
 
 @RestController
 public class PriceController {
 
     @GetMapping("/training-courses")
-    public List<Item> getTrainingCourses() {
-        return DataProcessor.list;
-    }
+    public List<Item> getTrainingCourses() { return DataProcessor.list; }
 
     @GetMapping("/report")
     public int getReport() {
         return DataProcessor.salesValue;
     }
 
-
-    @PutMapping("/training-courses")
+    @PutMapping("/update-training-course-prices")
     public void calculateTrainingCoursePrices() {
 
-        // TODO
-
+        DataProcessor.getInstance().calculateData();
     }
 
+    @PutMapping("/insert-training-courses")
+    public void insertTrainingCoursePrices(@RequestBody Item[] newItems) {
 
-
+        DataProcessor.getInstance().insertData(newItems);
+    }
 
 
 }
