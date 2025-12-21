6320062a8fd5bf3ce8bbd92d5970b0469c708254
Axel Wilhelm Berle <axelberle@yahoo.de>
2025-12-21 15:31:10 -0300

    consider empty results when retrieving training courses

--- src/test/java/com/workingagile/acsd/clientscripts/GetTrainingCoursesRestCommand.java
+++ src/test/java/com/workingagile/acsd/clientscripts/GetTrainingCoursesRestCommand.java
@@ -1,10 +1,7 @@
 package com.workingagile.acsd.clientscripts;
 
-import com.workingagile.acsd.backend.DataProcessor;
 import com.workingagile.acsd.backend.Item;
 import org.springframework.web.client.RestClient;
-
-import java.util.Arrays;
 
 public class GetTrainingCoursesRestCommand {
 
@@ -20,15 +17,17 @@ public class GetTrainingCoursesRestCommand {
             .baseUrl("http://localhost:8080")
             .build().get().uri("training-courses").retrieve().body(Item[].class);
 
-        for (Item item : items) {
-            System.out.println("Type: " + item.type);
-            System.out.println("When: " + item.trDate);
-            System.out.println("Remaining days before training course: " + item.days);
-            System.out.println("Full Price: " + item.full);
-            System.out.println("Current price: " + item.curr);
-            System.out.println("Number of seats: " + item.seats);
-            System.out.println("Remaining available seats: " + item.avail);
-            System.out.println();
+        if (items != null) {
+            for (Item item : items) {
+                System.out.println("Type: " + item.type);
+                System.out.println("When: " + item.trDate);
+                System.out.println("Remaining days before training course: " + item.days);
+                System.out.println("Full Price: " + item.full);
+                System.out.println("Current price: " + item.curr);
+                System.out.println("Number of seats: " + item.seats);
+                System.out.println("Remaining available seats: " + item.avail);
+                System.out.println();
+            }
         }
 
     }
