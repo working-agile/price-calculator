9ee8dc09a411de7c70bf929005af4545cb77c220
Axel Wilhelm Berle <axelberle@yahoo.de>
2026-01-08 11:22:35 -0300

    first example of an approval test

---
+++ src/test/java/com/workingagile/acsd/approval/Calculator.java
@@ -0,0 +1,10 @@
+package com.workingagile.acsd.approval;
+
+public class Calculator {
+
+    public int sum(int addend1, int addend2) {
+        return addend1 + addend2 +1;
+    }
+
+
+}
---
+++ src/test/java/com/workingagile/acsd/approval/CalculatorTest.java
@@ -0,0 +1,23 @@
+package com.workingagile.acsd.approval;
+
+import org.approvaltests.Approvals;
+import org.junit.jupiter.api.Test;
+
+public class CalculatorTest {
+
+    @Test
+    public void successful_sum() {
+
+        // Arrange
+        Calculator calculator = new Calculator();
+
+        // Act - call the function
+        int result = calculator.sum(10, 20);
+
+        // Assert
+        Approvals.verify("The expected result is: " + result);
+
+    }
+
+
+}
